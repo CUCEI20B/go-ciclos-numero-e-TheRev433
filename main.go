8b5ea390a0a1283510b301629a04863192e93b3c
Sergio Moreno <sergiomoreno433@gmail.com>
2020-09-30 20:24:14 -0500

    Get euler number

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var lim int
+	var e float64 = 0
+	fmt.Scanln(&lim)
+
+	for i := 0; i <= lim; i++ {
+		var fact float64 = 1
+		for j := 1; j <= i; j++ {
+			fact *= float64(j)
+		}
+		e += float64((float64(1) / fact))
+	}
+	fmt.Println(e)
 }
